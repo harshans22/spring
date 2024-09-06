fd7ddf452fc8f59675e38fa02c31821f68e8a7f8
harshans22 <harshhub.414@gmail.com>
2024-09-06 18:39:31 +0530

    how to set profile programmatically

--- src/main/java/com/learning/first/FirstApplication.java
+++ src/main/java/com/learning/first/FirstApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+import java.util.Collections;
+
+import static java.util.Collection.*;
+
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
-		var ctx=SpringApplication.run(FirstApplication.class, args);// to get context of IOC container
+        var app=new SpringApplication(FirstApplication.class);
+        app.setDefaultProperties(
+            Collections.singletonMap("spring.profiles.active","test")// manually setting the profile
+        );
+		var ctx=app.run(args);// to get context of IOC container
         MyFirstService myFirstService=ctx.getBean(MyFirstService.class);
         System.out.println(myFirstService.tellAStory());
         System.out.println(myFirstService.getCustomProperty());
